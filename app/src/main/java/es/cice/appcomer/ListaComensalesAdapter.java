7737de9cae29b91ff28595db159f1113fe03a8ab
jagarridoh <GetafeCice2017>
2017-05-08 19:15:31 +0200

    Añadido ListaComensalesAdapter

---
+++ app/src/main/java/es/cice/appcomer/ListaComensalesAdapter.java
@@ -0,0 +1,27 @@
+package es.cice.appcomer;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+/**
+ * Created by jagar on 08/05/2017.
+ */
+
+public class ListaComensalesAdapter extends ArrayAdapter {
+    public ListaComensalesAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+}
